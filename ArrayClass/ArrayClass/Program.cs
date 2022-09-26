a20d2b5623ce8f5271d86ea1d3bfbb0f3289277a
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-09-26 21:27:26 +0530

    add this file

---
+++ ArrayClass/ArrayClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Arrayclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4 };
+            Console.Write("Length of the array: ");
+            Console.Write(" " + a.Length);
+            Console.WriteLine();
+            Console.WriteLine("Given array");
+            foreach (int item in a)
+                Console.Write(" " + item);
+        }
+    }
+}
