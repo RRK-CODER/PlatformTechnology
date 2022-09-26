34ed607520fcea8400ca588707ed5a8529d58a28
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-09-26 21:41:32 +0530

    Add this file

---
+++ ArrayList/ArrayList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+namespace Arraylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList mylist = new ArrayList();
+            mylist.Add("My");
+            mylist.Add("name is");
+            mylist.Add("Rohit Ranjan Kumar ");
+            mylist.Add("I am in ");
+            mylist.Add("2020-2024");
+            mylist.Add("batch");
+            mylist.RemoveAt(5);
+            foreach (var item in mylist)
+                Console.WriteLine(item);
+        }
+    }
+}
