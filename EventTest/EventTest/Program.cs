ac4c7655398d7d78854cf78a85f0a227cdadde5f
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-10-15 21:34:23 +0530

    Add this file

---
+++ EventTest/EventTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+namespace EventTest
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press a to simulate a button click");
+            var key = Console.ReadLine();
+            if(key=="a")
+            {
+                KeyPressed();
+            }
+        }
+        static void KeyPressed()
+        {
+            Button button = new Button();
+            button.ClickEvent += (s, args) =>
+              {
+                  Console.WriteLine("You clicked a button");
+              };
+            button.OnClick();
+        }
+    }
+    public class Button
+    {
+        public EventHandler ClickEvent;
+        public void OnClick()
+        {
+            ClickEvent.Invoke(this, EventArgs.Empty);
+
+        }
+    }
+}
