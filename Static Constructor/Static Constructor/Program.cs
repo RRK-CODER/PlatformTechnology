1a4e20ebb2c1ba41b6fb041a921fa852b5097405
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-10-16 10:19:31 +0530

    add a new file

---
+++ Static Constructor/Static Constructor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Static_Constructor
+{
+    class User
+    {
+        //Static Constructor
+        static User()
+        {
+            Console.WriteLine("I am a static constructor");
+        }
+        //Default Constructor
+        public User()
+        {
+            Console.WriteLine("I am a default Constructor");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            User user1 = new User();
+            Console.WriteLine("\n Press Enter Key to Exit..");
+            Console.ReadLine();
+        }
+    }
+}
