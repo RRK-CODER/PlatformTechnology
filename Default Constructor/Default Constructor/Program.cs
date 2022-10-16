9c6b76b69f5fc6f3789de821f885d33eee748ec8
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-10-16 10:36:11 +0530

    Add this file

---
+++ Default Constructor/Default Constructor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Default_Constructor
+{
+    class addition
+        {
+            int a, b;
+            public addition()   //default contructor
+            {
+                a = 100;
+                b = 175;
+            }
+
+            public static void Main()
+            {
+                addition obj = new addition(); //an object is created , constructor is called
+                Console.WriteLine(obj.a);
+                Console.WriteLine(obj.b);
+                Console.Read();
+            }
+        }
+    }
+
+
