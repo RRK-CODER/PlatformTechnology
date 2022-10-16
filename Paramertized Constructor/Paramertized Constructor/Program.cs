b6fa06a247b93ea8222abbf244064f35404c352b
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-10-16 10:42:52 +0530

    add this file

--- Paramertized Constructor/Paramertized Constructor/Program.cs
+++ Paramertized Constructor/Paramertized Constructor/Program.cs
@@ -2,11 +2,27 @@
 
 namespace Paramertized_Constructor
 {
-    class Program
-    {
-        static void Main(string[] args)
+   class paraconstrctor
+   {
+            public int a, b;
+            public paraconstrctor(int x, int y)
+            {
+                a = x;
+                b = y;
+            }
+   }
+        class MainClass
         {
-            Console.WriteLine("Hello World!");
+            static void Main()
+            {
+                paraconstrctor v = new paraconstrctor(100, 175);
+                Console.WriteLine("parameterized constructor example");
+                Console.WriteLine("\t");
+                Console.WriteLine("value of a=" + v.a);
+                Console.WriteLine("value of b=" + v.b);
+                Console.Read();
+            }
         }
-    }
-}
+ }
+
+
