df064b261944a70bd25133db9c5b6474fcecc045
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-10-16 10:39:33 +0530

    add this file

---
+++ Paramertized Constructor/Paramertized Constructor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Paramertized_Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
