e415b1fd9262dd9ee3c14ffd12ee1d2e1035eb7c
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-10-16 12:19:02 +0530

    add this file

---
+++ Threading/Threading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+namespace Threading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create child thread
+              Thread t1 = new Thread(new ThreadStart(PrintInfo));
+            // start newly created thread
+               t1.Start();
+            Console.WriteLine("Main Thread Completed");
+            Console.ReadLine();
+        }
+        static void PrintInfo()
+        {
+            for(int i=1; i<=4; i++)
+            {
+                Console.WriteLine("ivalur:{0}", i);
+            }
+            Console.WriteLine("Child Thread Completed");
+        }
+    }
+}
