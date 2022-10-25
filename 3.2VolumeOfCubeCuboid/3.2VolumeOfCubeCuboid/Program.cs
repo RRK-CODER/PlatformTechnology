7b2cd03f2a7dc0bd66f25c1a52466e28098e4427
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-10-25 14:10:49 +0530

    Add this file

---
+++ 3.2VolumeOfCubeCuboid/3.2VolumeOfCubeCuboid/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class FindVolume
+{
+    public void VolumeDisplay(int r)
+    {
+        Console.WriteLine("Volume of cube {0}", (r * r * r));
+    }
+    public void VolumeDisplay(int l, int b, int h)
+    {
+        Console.WriteLine("Volume of cuboid {0}", (l * b * h));
+    }
+}
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        FindVolume f = new FindVolume();
+        f.VolumeDisplay(4);
+        f.VolumeDisplay(4, 5, 6);
+    }
+}
