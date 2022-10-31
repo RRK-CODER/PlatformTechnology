b6457f2d252bacae4cdc9eb86ce6d9a87c38cce1
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-10-31 16:07:27 +0530

    add this file

---
+++ 2.2Bill(ConstructionOverloading)/2.2Bill(ConstructionOverloading)/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Transactions;
+using System.Xml.Schema;
+
+class bill
+{
+    string[] item = new string[5];
+    double[] price = new double[5];
+    int[] quantity = new int[5];
+    double[] sum = new double[5];
+    string date;
+
+
+    public bill()
+    {
+        Console.WriteLine("Enter the date:");
+        date = Console.ReadLine();
+        Console.WriteLine("\n\n Enter the product, price and quantity");
+        for(int i=0; i<item.Length;i++ )
+        {
+            Console.Write("\n\n Product {0}: ", i + 1);
+            item[i] = Console.ReadLine();
+            Console.Write(" " + " Price:");
+            price[i] = double.Parse(Console.ReadLine());
+            Console.Write(" " + "Qauntity:");
+            quantity[i] = int.Parse(Console.ReadLine());
+        }
+
+    }
+    public bill (bill temp)
+    {
+        item = temp.item;
+        price = temp.price;
+        quantity = temp.quantity;
+        date = temp.date;
+        double total = 0;
+        for(int i=0; i<price.Length;i++)
+        {
+            sum[i] = (price[i] * quantity[i]);
+            total += sum[i];
+        }
+        Console.WriteLine("\n\nDATE:" + date);
+        Console.WriteLine("\n\n " + "PRODUCT NAME \t\t" + "QUANTITY\t\t" + "PRICE\t\t" + "TOTAL PRICE \n\n");
+        for(int i=0; i<item.Length;i++)
+        {
+            Console.WriteLine(" " + item[i] + "\t\t\t\t" + quantity[i] + "\t\t" + price[i] + "\t\t" + sum[i]);
+        }
+        Console.WriteLine("\n\n TOTAL AMOUNT: " + total);
+    }
+}
+
+namespace _2._2Bill_ConstructionOverloading_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bill b = new bill();
+            bill b1 = new bill(b);
+        }
+    }
+}
